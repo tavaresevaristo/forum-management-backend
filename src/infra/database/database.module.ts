0a102c6918beed4093a50f1bfae63c152c5f41b2
Tavares Evaristo <tavaresevaristo07@gmail.com>
2024-08-04 12:47:21 +0100

    feat: implemented prisma repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaAnswerRepository } from './prisma/repositories/prisma-answer-repository';
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository';
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository';
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository';
+import { PrismaQuestionsCommentsRepository } from './prisma/repositories/prisma-question-comments.repository';
+import { PrismaQuestionsAttachmentsRepository } from './prisma/repositories/prisma-question-attachments.repository';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    PrismaAnswerRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaQuestionsRepository,
+    PrismaQuestionsCommentsRepository,
+    PrismaQuestionsAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaAnswerRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaQuestionsRepository,
+    PrismaQuestionsCommentsRepository,
+    PrismaQuestionsAttachmentsRepository,
+  ],
 })
 export class DatabaseModule {}
